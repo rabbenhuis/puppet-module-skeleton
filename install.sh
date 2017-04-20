24ebb04119fcdef47aabb7f1c294522da2598f8b
Richard Abbenhuis <richard.abbenhuis@tennet.eu>
2017-04-20 15:48:02 +0200

    Copy all files to install_dir and then remove .gitkeep files from /skeleton

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 install_dir=$(puppet apply --configprint module_working_dir)/
 
-find skeleton -type f -not -name .gitkeep | git checkout-index --stdin --force --prefix=$install_dir
+find skeleton -type f | git checkout-index --stdin --force --prefix=$install_dir
+find ${install_dir}/skeleton -type f -name .gitkeep -exec rm -f {} \;
